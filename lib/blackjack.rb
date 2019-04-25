fa145bbc02f29e82442dc1fe73a63106b7d4ce34
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 17:46:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,6 +52,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total => 21 do
+    card_total = hit(card_total)
+  end
+end_game
 end
     
